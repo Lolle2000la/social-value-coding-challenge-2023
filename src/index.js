ef232acb0426d8a68e0e024890a40e9c5fb41865
Luca Auer <lolle2000.la@gmail.com>
2023-08-06 14:05:24 +0200

    Initialize project dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import '@fontsource/source-sans-pro';
+import '@fontsource-variable/signika';
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
